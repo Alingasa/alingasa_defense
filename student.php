30c7eec5e184bade548cc8dafc49cce66c6456d8
James Alingasa <James>
2023-05-27 11:21:21 +0800

    commit

---
+++ student.php
@@ -0,0 +1,21 @@
+<?php
+ include "main.php";
+ 
+class Student extends Db implements Manage
+{
+    public $tblName = "Students";
+
+    public function createtbl()
+    {
+        $this->connect();
+        $createtbl = "CREATE TABLE IF NOT EXISTS $this->tblName(
+            id int auto_increment primary key,
+            first_name text,
+            middle_name text,
+            last_name text)";
+
+            $this->conn->query($createtbl);
+            var_dump($this->conn->error);
+
+    }
+}
