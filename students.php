ff343f15d9ba83186691b56071322c0b4e46ecb5
Alingasa <130818761+Alingasa@users.noreply.github.com>
2023-06-03 01:22:35 +0800

    Update students.php

--- students.php
+++ students.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-type: application/json; charset=UTF-8");
 
-include "class/interface.php";
+include "classes/interface.php";
 include "databaseconnection.php";
 class Students extends Database implements Dbconn
 {
